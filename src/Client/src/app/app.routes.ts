a3a4e49bcd6d00d46eca07b38564210648196da6
David Castelli <david.castelli@mail.mcgill.ca>
2024-07-13 22:27:22 -0400

    Added landing page

--- src/Client/src/app/app.routes.ts
+++ src/Client/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './core/auth/pages/register/register.component';
 import { LoginComponent } from './core/auth/pages/login/login.component';
+import { LandingComponent } from './core/layout/pages/landing/landing.component';
 
 export const routes: Routes = [
+  {
+    path: 'landing',
+    component: LandingComponent,
+    title: 'Landing Page',
+  },
   {
     path: 'register',
     component: RegisterComponent,
@@ -12,5 +18,10 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent,
     title: 'Login Page',
+  },
+  {
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full',
   },
 ];
