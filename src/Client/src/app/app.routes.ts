9e3881f656bbfc8d1af586466b30869e4f62cf17
David Castelli <david.castelli@mail.mcgill.ca>
2024-07-14 14:41:09 -0400

    Added error page for page not found

--- src/Client/src/app/app.routes.ts
+++ src/Client/src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './core/auth/pages/register/register.component';
 import { LoginComponent } from './core/auth/pages/login/login.component';
+import {PageNotFoundComponent} from "./core/errors/pages/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
-    title: 'Registration Page',
+    title: 'Register',
   },
   {
     path: 'login',
     component: LoginComponent,
-    title: 'Login Page',
+    title: 'Login',
   },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    title: 'PageNotFound'
+  }
 ];
