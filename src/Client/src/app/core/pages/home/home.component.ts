75edeb8efe9d6f5bb64531a80df1d613fbfe1a94
David Castelli <david.castelli@mail.mcgill.ca>
2025-05-30 15:52:23 -0400

    Added home page

--- src/Client/src/app/core/pages/home/home.component.ts
+++ src/Client/src/app/core/pages/home/home.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {slideAnimation} from "../../../shared/animations/slide.animation";
+import {SidebarService} from "../../layout/sidebar.service";
+import {AsyncPipe} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+  ],
+  animations: [slideAnimation],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private readonly sidebarService = inject(SidebarService);
+  private readonly router = inject(Router);
+
+  public readonly isSidebarOpen$ = this.sidebarService.openClose$;
+
+  navigateRecipes() {
+    void this.router.navigate(['/manage/recipes']);
+  }
+
+  navigateMeals() {
+    void this.router.navigate(['/manage/meals']);
+  }
+
+  navigateSchedule() {
+    void this.router.navigate(['/schedule']);
+  }
+
+  navigateShoppingList() {
+    void this.router.navigate(['/shopping-list']);
+  }
+}
